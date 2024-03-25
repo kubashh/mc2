cbf553ac3006dc2c712b9ce07220e4a17ef87eb1
kubashh <146523311+kubashh@users.noreply.github.com>
2024-03-25 13:27:45 +0100

    Create game.js

---
+++ game.js
@@ -0,0 +1,7 @@
+const canvas = document.createElement('canvas');
+canvas.width = 1920;
+canvas.height = 1080;
+
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
