98a83c426a22390d8d234e51ac1e51c5462cb0b2
kubashh <146523311+kubashh@users.noreply.github.com>
2024-03-25 12:51:59 +0000

    1.0

--- game.js
+++ game.js
@@ -5,3 +5,29 @@ canvas.height = 1080;
 const ctx = canvas.getContext('2d');
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+
+const pixelSize = 10;
+
+
+class Mob {
+  isPlayer = false;
+}
+
+class Block {
+  constructor(x, y, color) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+  }
+
+  draw = () => {
+    drawBox(self.x * pixelSize, self.y * pixelSize, pixelSize, pixelSize, this.color);
+  }
+}
+
+class Item {
+
+}
+
+let block = new Block(3, 3, "red");
+block.draw();
